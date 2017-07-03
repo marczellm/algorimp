0e13e4e231c579ff3e1253bab3eb2d8402561148
marczellm <marczell.marton@hallgato.ppke.hu>
2017-07-03 17:09:52 +0200

    Data and command binding

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -1,22 +1,13 @@
 from tkinter import filedialog
 import gui.lib
-from gui.lib import observable
 
 
 class MainWindow(gui.lib.BaseWindow):
     def __init__(self):
         super().__init__()
-        self._training_filename = ""
+        self._training_filename = ''
+        self._training_set_type = ''
         self.title = 'AlgorImp'
-
-    @observable(str)
-    @property
-    def training_filename(self) -> str:
-        return self._training_filename
-
-    @training_filename.setter
-    def training_filename(self, val):
-        self._training_filename = val
 
     def browse_button_onclick(self):
         self.training_filename = filedialog.askopenfilename(filetypes=[('MIDI file', '*.mid *.smf')])
--- gui/lib/__init__.py
+++ gui/lib/__init__.py
@@ -21,39 +21,76 @@ def register(cls: type):
 
 
 class BaseComponent:
+    __type_mapping = {
+        int: tk.IntVar,
+        str: tk.StringVar,
+        bool: tk.BooleanVar,
+        float: tk.DoubleVar
+    }
+
     def __new__(cls: type):
         register(cls)
         return super().__new__(cls)
 
+    def __init__(self):
+        self.__bound_variables = {}
+
+    def construct(self, elem: Xml.Element, parent: Union[tk.Widget, tk.Tk]):
+        init_args = {k: v for k, v in elem.attrib.items() if '-' not in k}
+        pack_args = {k[5:]: v for k, v in elem.attrib.items() if 'pack-' in k}
+        grid_args = {k[5:]: v for k, v in elem.attrib.items() if 'grid-' in k}
+        place_args = {k[6:]: v for k, v in elem.attrib.items() if 'place-' in k}
+
+        widget_class = _widgets[elem.tag]
+        if elem.text and elem.text.strip():
+            init_args['text'] = elem.text.strip()
+
+        for key, membername in init_args.items():
+            if 'command' in key:
+                init_args[key] = getattr(self, membername)
+            if 'variable' in key:
+                var = None
+                if membername in self.__bound_variables:
+                    var = self.__bound_variables[membername]
+                else:
+                    private_membername = '_' + membername
+                    value = getattr(self, private_membername)
+                    typ = type(value)
+                    var = self.__type_mapping[typ]()
+                    if typ not in self.__type_mapping.keys():
+                        raise Exception('Binding to {} not supported'.format(typ))
+
+                    def getter(this):
+                        return getattr(this, private_membername)
+
+                    def setter(this, val):
+                        setattr(this, private_membername, val)
+                        var.set(val)
+
+                    setattr(type(self), membername, property(getter, setter))
+                    var.trace_add('write', lambda *_: setattr(self, private_membername, var.get()))
+                    self.__bound_variables[membername] = var
+                init_args[key] = var
+
+        widget = widget_class(parent, **init_args)
+        for child in elem:
+            self.construct(child, widget)
+
+        if pack_args:
+            widget.pack(**pack_args)
+        elif grid_args:
+            widget.grid(**grid_args)
+        elif place_args:
+            widget.place(**place_args)
+        else:
+            widget.pack()
+        return widget
+
 
 class BaseWindow(BaseComponent):
     def __init__(self):
+        super().__init__()
         self.title = ''
-
-
-def construct(elem: Xml.Element, parent: Union[tk.Widget, tk.Tk]):
-    init_args = {k: v for k, v in elem.attrib.items() if '-' not in k}
-    pack_args = {k[5:]: v for k, v in elem.attrib.items() if 'pack-' in k}
-    grid_args = {k[5:]: v for k, v in elem.attrib.items() if 'grid-' in k}
-    place_args = {k[6:]: v for k, v in elem.attrib.items() if 'place-' in k}
-
-    widget_class = _widgets[elem.tag]
-    if elem.text and elem.text.strip():
-        init_args['text'] = elem.text.strip()
-
-    widget = widget_class(parent, **init_args)
-    for child in elem:
-        construct(child, widget)
-
-    if pack_args:
-        widget.pack(**pack_args)
-    elif grid_args:
-        widget.grid(**grid_args)
-    elif place_args:
-        widget.place(**place_args)
-    else:
-        widget.pack()
-    return widget
 
 
 def show(window: BaseWindow):
@@ -61,11 +98,5 @@ def show(window: BaseWindow):
     tree = Xml.parse(_registry[name])
     hwnd = tk.Tk()
     hwnd.wm_title(window.title)
-    widget = construct(tree.getroot(), hwnd)
+    widget = window.construct(tree.getroot(), hwnd)
     widget.mainloop()
-
-
-def observable(typ):
-    def ret(prop):
-        return prop
-    return ret
