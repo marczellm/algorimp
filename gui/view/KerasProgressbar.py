33fd2fb4fd84855156bc54d9e48bc469c2ec5cb8
marczellm <marczell.marton@hallgato.ppke.hu>
2017-07-08 13:27:42 +0200

    even more simpler

--- gui/view/KerasProgressbar.py
+++ gui/view/KerasProgressbar.py
@@ -6,6 +6,3 @@ class KerasProgressbar(BaseComponent):
     template = '''
     <Progressbar name="progressbar" variable="[value]" maximum="[target]"/>
     '''
-
-    def __init__(self, parent, **kwargs):
-        super().__init__(parent, **kwargs)
